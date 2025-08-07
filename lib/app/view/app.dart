8332b1e9eb077a9302d746f6aed8015b56508a75
Umang Jindal <umangkkr@gmail.com>
2025-08-07 15:29:10 +0530

    profile screen ui

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:snack_track_app/l10n/l10n.dart';
+import 'package:profile_view/home_screen.dart';
+
 import 'package:snack_track_app/profile/profile.dart';
 import 'package:snack_track_app/theme/app_theme.dart';
 
@@ -13,7 +15,7 @@ class App extends StatelessWidget {
       theme: AppTheme.light,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const ProfilePage(),
+      home: const FitnessProfileScreen(),
     );
   }
 }
