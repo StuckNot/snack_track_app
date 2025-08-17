f398308e9aa06eac947b1439b497d7e67afd3cb8
StuckNot <info@stucknot.com>
2025-08-17 16:06:35 +0530

    feat: padding and sized box

---
+++ packages/authenticate/lib/src/presentation/theme/auth_padding.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AuthPadding{
+
+  static const EdgeInsets page = EdgeInsets.all(20);
+
+}
+
+class AuthSizedBox{
+  static const SizedBox height = SizedBox(height: 20);
+  static const SizedBox width = SizedBox(width: 20);
+}
