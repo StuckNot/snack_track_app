88c1d4e5d9736eed866239deabb603c404fe59d4
StuckNot <info@stucknot.com>
2025-08-17 16:02:31 +0530

    feat: bdd for auth

---
+++ packages/authenticate/test/src/step/i_land_on_login_screen_with_correct_data.dart
@@ -0,0 +1,20 @@
+import 'package:authenticate/authenticate.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+/// Usage: I land on Login Screen with correct data
+Future<void> iLandOnLoginScreenWithCorrectData(WidgetTester tester) async {
+  await tester.pumpWidget(
+    const MaterialApp(
+      title: 'SnackTrack',
+      localizationsDelegates: [
+        ...AuthenticateLocalization.localizationsDelegates,
+      ],
+      supportedLocales: [
+        ...AuthenticateLocalization.supportedLocales,
+      ],
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    ),
+  );
+}
